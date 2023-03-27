b778b7f6f46dd1238c0a48db1221b3667120675c
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2023-03-27 17:08:06 +0700

    add test line message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('test/line-message')
+  async testLineMessage(@Body() message: { message: string }) {
+    return await this.appService.testLineMessage(message.message);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { AlertService } from './alert/alert.service';
+import { LineChatbotService } from './line-chatbot/line-chatbot.service';
+import { LineService } from './line/line.service';
 
 @Injectable()
 export class AppService {
+  constructor(private lineService: LineChatbotService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async testLineMessage(message: string) {
+    await this.lineService.pushMessage([
+      {
+        text: message,
+        type: 'text',
+      },
+    ]);
   }
 }
